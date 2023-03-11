05085cd1d587fe665c1b90b749d90ca47911da72
EKI-INDRADI <eki.indradi@gmail.com>
2023-03-11 20:45:32 +0700

    update

---
+++ eki_fe_b_react/src/root.component.tsx
@@ -0,0 +1,12 @@
+export default function Root(props) {
+
+  // ------------default
+  // return <section>{props.name} is mounted!</section>;
+  // ------------default
+
+    return <section>
+      <h1>{props.name} is mounted!</h1>
+      <h2>this is a sample text!</h2>
+    </section>;
+  
+}
